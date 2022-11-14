2e926ebe756b7e17ddf0f96a707cd824ab53719b
alina1412 <alina1412@mail.ru>
2022-11-14 07:37:36 +0300

    test addition

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,12 +7,12 @@ from bot_service.config import Config
 # @pytest.mark.my
 async def test_get_weather_data():
     assert Config.weather_token is not None
-    city = "Moscow"
-    url = "http://api.openweathermap.org" + f"/data/2.5/weather?q={city}"
-    params = {
-        "units": "metric",
-        "appid": Config.weather_token,
-    }
-    async with httpx.AsyncClient() as client:
-        resp = await client.get(url, params=params)
-    assert resp.status_code == 200
+    for city in ("Moscow", "yoshkar-ola", "saint petersburg"):
+        url = "http://api.openweathermap.org" + f"/data/2.5/weather?q={city}"
+        params = {
+            "units": "metric",
+            "appid": Config.weather_token,
+        }
+        async with httpx.AsyncClient() as client:
+            resp = await client.get(url, params=params)
+        assert resp.status_code == 200
